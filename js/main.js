8fcdd852ec0b87379da58077c5ab8abe1a151cd4
Kate K <kilina2018@bk.ru>
2020-05-21 15:20:35 +0400

    feat: add form mask and validation

--- js/main.js
+++ js/main.js
@@ -31,5 +31,73 @@ var mySwiper = new Swiper('.swiper-container', {
 
   new WOW().init();
 
+  //Валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Не менее двух букв"
+      },
+      userPhone: "Номер телефона обязателен",
+      userEmail: {
+        required: "Email обязателен",
+        email: "Ваш email должен быть формата name@domain.com"
+      }
+    }
+  });
+
+  $('.control__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required"
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Не менее двух букв"
+      },
+      userPhone: "Номер телефона обязателен"
+    }
+  });
+
+  $('.footer__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      userQuestion: "required"
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Не менее двух букв"
+      },
+      userPhone: "Номер телефона обязателен",
+      userQuestion: "Введите вопрос"
+    }
+  });
+
+  //маска
+  $('[type=tel]').mask('+7(000) 000-00-00', { placeholder: "+7 (___) ___-__-__"});
+
 });
 
