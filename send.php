fab278e3b7c90bb89355e6e4b3abf307003d5420
Kate K <kilina2018@bk.ru>
2020-05-24 17:38:07 +0400

    feat: add php files, thanks page

---
+++ send.php
@@ -0,0 +1,41 @@
+<?php
+
+  $userName = $_POST['userName'];
+  $userEmail = $_POST['userEmail'];
+  $userPhone = $_POST['userPhone'];
+
+
+// Load Composer's autoloader
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->CharSet = 'UTF-8';
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ekaterinakilna55@gmail.com';                     // SMTP username
+    $mail->Password   = 'formsender090@';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('ekaterinakilna55@gmail.com', 'Катя');
+    $mail->addAddress('kilina2018@bk.ru');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, телефон: ${userPhone}, email: ${userEmail}";
+
+    $mail->send();
+    header('location: thanks.html');
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
