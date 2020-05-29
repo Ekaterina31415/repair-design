b584cf4b2f3197d5c9acf4a8909f0ae69d1e37e9
Kate K <kilina2018@bk.ru>
2020-05-29 17:41:42 +0400

    refact: fixed pp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+var tinypng = require('gulp-tinypng-compress');
 
 // Static server
 function bs() {
@@ -16,7 +20,7 @@ function bs() {
   watch("./sass/**/*.scss", serveSass);
   watch("./js*.js").on('change', browserSync.reload);
 
-};
+}
 
 function serveSass() {
   return src("./sass/**/*.sass", "./sass/**/*.scss")
@@ -26,6 +30,56 @@ function serveSass() {
     }))
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
-};
+}
+
+function buildCSS(done) {
+  src('css/**/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(dest('dist/css/'));
+  done();
+}
+
+function buildJS(done) {
+  src(['js/*.js', '!js/*.min.js'])
+    .pipe(minify({
+          ext: {
+            min: '.js'
+          }}))
+    .pipe(dest('dist/js/'));
+  src(['js/**.min.js']).pipe(dest('dist/js/'));
+  done();
+}
+
+function html(done) {
+  src(['**html'])
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('dist/'));
+  done();
+}
+
+function php(done) {
+  src('php/phpmailer/**')
+    .pipe(dest('dist/php/phpmailer/'));
+  src('php/**').pipe(dest('dist/php/'));
+  done();
+}
+
+function fonts(done) {
+  src('fonts/futura/**')
+    .pipe(dest('dist/fonts/'));
+  done();
+}
+
+function imagemin(done) {
+  src('img/**/**')
+    .pipe(tinypng({
+        key: 'MpWbV1QCNlJDTRNHnfGCS2GkG9s774rc'
+      }))
+      .pipe(gulp.dest('img'));
+  src('img/**/*.svg')
+    .pipe(gulp.dest('img'));
+  done();
+}
 
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imagemin);
